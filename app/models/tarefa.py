232538e76ef5f3d229263d8f884266f86f55a985
junioranheu <junioranheu@gmail.com>
2026-08-22 20:47:47 -0300

    feat: add one-to-many relationship between Usuario and Tarefa

--- app/models/tarefa.py
+++ app/models/tarefa.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship # <-- Importe relationship
+from sqlalchemy import String, Boolean, ForeignKey             # <-- Importe ForeignKey
 from app.database import Base
 
 # Equivalente a uma classe de Entidade no EF Core
@@ -10,3 +10,9 @@ class Tarefa(Base):
     titulo: Mapped[str] = mapped_column(String(100))
     descricao: Mapped[str | None] = mapped_column(String(255), nullable=True)
     concluida: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # Chave estrangeira ligando à tabela de usuários
+    usuario_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
+    
+    # Navegação (Não vira coluna no banco, é só para o Python)
+    criador: Mapped["Usuario"] = relationship(back_populates="tarefas")
--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship # <-- Importe relationship
 from sqlalchemy import String
 from app.database import Base
 
@@ -8,3 +8,6 @@ class Usuario(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(150), unique=True, index=True)
     senha_hash: Mapped[str] = mapped_column(String(255))
+
+    # Navegação: Uma lista de tarefas vinculadas a este usuário
+    tarefas: Mapped[list["Tarefa"]] = relationship(back_populates="criador")
